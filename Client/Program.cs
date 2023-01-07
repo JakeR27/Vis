9625bb1032d2e7e37ec4847a1a235c572641b15a
Jake <Jake@MATEBOOK>
2023-01-07 17:14:57 +0000

    #3 implemented client

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,9 +1,57 @@
-﻿Vis.Constants.BACKEND_XCH;
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using Vis.Client.Consumers;
+using Vis.Common;
 
-class Main
+namespace Vis.Client
 {
-    public static void Main()
+    class Program
     {
-        Vis.Common
+        private static IModel _channel;
+
+        public static void Main()
+        {
+            var factory = new ConnectionFactory()
+                { HostName = "ec2-13-42-23-89.eu-west-2.compute.amazonaws.com", UserName = "backend", Password = "backend" };
+
+
+            var connection = factory.CreateConnection();
+            _channel = connection.CreateModel();
+            Vis.Common.Publishers.SafePublisher.useChannel(_channel);
+
+        
+            var createQueue = _channel.QueueDeclare().QueueName;
+            var inQueue = _channel.QueueDeclare().QueueName;
+            var outQueue = _channel.QueueDeclare().QueueName;
+            var hostQueue = _channel.QueueDeclare().QueueName;
+            var authQueue = _channel.QueueDeclare().QueueName;
+
+            string hostRequestsRoutingKey = $"{ClientData._organisationId}.{ClientData._unitId}.requests.host";
+            string authRequestsRoutingKey = $"{ClientData._organisationId}.{ClientData._unitId}.requests.auth";
+            string hostResponsesRoutingKey = $"{ClientData._organisationId}.{ClientData._unitId}.responses.host";
+            string authResponsesRoutingKey = $"{ClientData._organisationId}.{ClientData._unitId}.responses.auth";
+
+
+            _channel.ExchangeDeclare(exchange: "discovery-xch", type: ExchangeType.Topic);
+            _channel.QueueBind(queue: hostQueue, exchange: "discovery-xch", routingKey: hostResponsesRoutingKey);
+            _channel.QueueBind(queue: authQueue, exchange: "discovery-xch", routingKey: authResponsesRoutingKey);
+            
+            new AuthConsumer().Attach(_channel, authQueue);
+
+            Publishers.SafePublisher.send(exchange: Constants.DISCOVERY_XCH, routingKey: authRequestsRoutingKey, body: Encoding.UTF8.GetBytes("CREATE MESSAGE"));
+
+            while (!ClientData._organisationExchangeFound) { } //wait
+
+            _channel.ExchangeDeclare(exchange: ClientData._organisationExchangeName, type: ExchangeType.Topic);
+            _channel.QueueBind(queue: createQueue, exchange: ClientData._organisationExchangeName, routingKey: "*.create");
+            _channel.QueueBind(queue: inQueue,     exchange: ClientData._organisationExchangeName, routingKey: "*.in");
+            _channel.QueueBind(queue: outQueue,    exchange: ClientData._organisationExchangeName, routingKey: "*.out");
+
+            Publishers.SafePublisher.send(exchange: ClientData._organisationExchangeName, routingKey: $"{ClientData._organisationId}.create", body: Encoding.UTF8.GetBytes("CREATE message content"));
+            Publishers.SafePublisher.send(exchange: ClientData._organisationExchangeName, routingKey: $"{ClientData._organisationId}.in",     body: Encoding.UTF8.GetBytes("IN message content"));
+            Publishers.SafePublisher.send(exchange: ClientData._organisationExchangeName, routingKey: $"{ClientData._organisationId}.out",    body: Encoding.UTF8.GetBytes("OUT message content"));
+
+        }
     }
 }
