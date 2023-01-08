ef6ad9a54cb31c06243f62716e81371d3d245ccd
Jake <Jake@MATEBOOK>
2023-01-08 19:57:50 +0000

    corrected strings

--- Common/Constants.cs
+++ Common/Constants.cs
@@ -8,8 +8,8 @@ namespace Vis
         public const string BACKEND_XCH = "backend-xch";
         public static string ORGANISATION_XCH(int id) => $"org-{id}-xch";
         public static string AUTH_RESPONSE_KEY(int orgId, int unitId) => $"{orgId}.{unitId}.responses.auth";
-        public static string AUTH_REQUEST_KEY(int orgId, int unitId) => $"{orgId}.{unitId}.responses.auth";
-        public static string HOST_RESPONSE_KEY(int orgId, int unitId) => $"{orgId}.{unitId}.requests.host";
+        public static string AUTH_REQUEST_KEY(int orgId, int unitId) => $"{orgId}.{unitId}.requests.auth";
+        public static string HOST_RESPONSE_KEY(int orgId, int unitId) => $"{orgId}.{unitId}.responses.host";
         public static string HOST_REQUEST_KEY(int orgId, int unitId) => $"{orgId}.{unitId}.requests.host";
         public static string BODY_AS_TEXT(dynamic b) => Encoding.UTF8.GetString(b.ToArray());
     }
