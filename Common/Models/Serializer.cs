60f83201c287f6363d25a90f827bb3e2e4c062dc
Jake <Jake@MATEBOOK>
2023-01-08 19:52:59 +0000

    #5 implemented serialization to json and byte arr

---
+++ Common/Models/Serializer.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Text;
+using System.Text.Json;
+
+namespace Vis.Common.Models
+{
+    public static class Serializer
+    {
+        static readonly JsonSerializerOptions Options = new() { IncludeFields = true };
+        public static byte[] Serialize<T>(T obj)
+        {
+            var JsonString = System.Text.Json.JsonSerializer.Serialize(obj, Options);
+            Logs.Log(Logs.LogLevel.Debug, JsonString);
+            return Encoding.UTF8.GetBytes(JsonString);
+        }
+
+        public static T Deserialize<T>(byte[] bytes)
+        {
+            var JsonString = Encoding.UTF8.GetString(bytes);
+            Logs.Log(Logs.LogLevel.Debug, JsonString);
+            return System.Text.Json.JsonSerializer.Deserialize<T>(JsonString, Options)!;
+        }
+    }
+}
